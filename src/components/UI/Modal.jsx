f762370a67328e5dae2fb521d4c3dc0f7266fd0c
arifwp <arifwahyu696@gmail.com>
2024-05-08 14:56:34 +0700

    add modal

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -5,16 +5,16 @@ export default function Modal({ children, open, className = '' }) {
     const dialog = useRef();
 
     useEffect(() => {
+        const modal = dialog.current;
         if (open) {
-            dialog.current.showModal();
+            modal.showModal();
         }
+
+        return () => modal.close();
     }, [open]);
 
     return createPortal(
-        <dialog
-            ref={dialog}
-            className={`modal ${className}`}
-        >
+        <dialog ref={dialog} className={`modal ${className}`}>
             {children}
         </dialog>,
         document.getElementById('modal')
